0cc3eb2435b3f240183d79b2824388f1f31677ca
PatricioEstrada <92997422+PatricioEstrada@users.noreply.github.com>
2021-10-22 13:20:30 -0500

    Add files via upload

---
+++ script_01.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Date: 2021-10-22
+# @autor: Patricio Estrada
+
+nombre = 'Patricio Estrada' # variable nombre
+print('Empezando a trabajar con Python') # texto en pantalla
+print(f'Realizado por \'{nombre}\'') # texto en pantalla
+
+print('Consultando los tipos de variables:') # texto en pantalla
+
+print('El tipo de dato de 875 es: \n {}'.format(type(875))) # texto con formato en pantalla
+
+print('El tipo de dato de 4.89 es: \n {}'.format(type(4.89))) # texto con formato en pantalla
+
+print('El tipo de dato del texto: Now is better than never. es: \n {}'.format(type('Now is better than never.'))) # texto con formato en pantalla
+
+print('El tipo de dato de 1.32 es: \n {}'.format(type(1.32))) # texto con formato en pantalla
+
+print('¿El valor 5 + 8i corresponde a un valor entero?')
+print(type(5 + 8j))
+
+print('¿El valor 8.2 corresponde a un valor entero?')
+print(type(8.2))
+
+print('¿El texto: Readability counts. corresponde a un string?')
+print(type('Readability counts.'))
+
